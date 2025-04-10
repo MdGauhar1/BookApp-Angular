5903fdc5e9c1904b3d93928eba0f60a818f3471c
gauhar <mdgauhar0007@gmail.com>
2025-04-10 14:39:41 +0530

    secound

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { BookListComponent } from '../book-list/book-list.component';
+import { AddBookComponent } from '../add-book/add-book.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
