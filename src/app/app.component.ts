2904a387c4d0bb7646c5eaaebd40693788c7766b
gauhar <mdgauhar0007@gmail.com>
2025-04-11 12:30:03 +0530

    n3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, RouterLink],
-  template: `<router-outlet></router-outlet>`,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'book-app';
+
+  toggleDarkMode(event: any) {
+    const checked = event.target.checked;
+    if (checked) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
 }
