5774c00ef4b6c79294215d3f63bad3b4dcd842f0
gauhar <mdgauhar0007@gmail.com>
2025-04-11 09:59:03 +0530

    n

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BookService } from '../services/book.service';
+import { Book } from '../models/book.model';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-add-book',
-  imports: [],
+  standalone: true,
   templateUrl: './add-book.component.html',
-  styleUrl: './add-book.component.css'
+  styleUrls: ['./add-book.component.css'],
+  imports: [FormsModule]
 })
 export class AddBookComponent {
+  book: Book = { id: 0, title: '', author: '' };
 
+  constructor(private bookService: BookService, private router: Router) {}
+
+  onSubmit(): void {
+    this.bookService.addBook(this.book).subscribe(() => {
+      alert('Book added successfully!');
+      this.router.navigate(['/book-list']);
+    });
+  }
 }
