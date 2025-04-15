64322bf8f65f293b2130e4ccb53d0400d511978f
gauhar <mdgauhar0007@gmail.com>
2025-04-15 13:08:44 +0530

    n6

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { BookService } from '../services/book.service';
 import { Book } from '../models/book.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -16,7 +17,9 @@ export class SearchComponent {
   query: string = '';
   books: Book[] = [];
 
-  constructor(private bookService: BookService) {}
+  //constructor(private bookService: BookService) {}
+  constructor(private bookService: BookService, private http: HttpClient) {}
+
 
   search() {
     if (this.searchType === 'title') {
@@ -24,6 +27,19 @@ export class SearchComponent {
     } else if (this.searchType === 'author') {
       this.bookService.searchByAuthor(this.query).subscribe(data => this.books = data);
     }
+  }
+
+  downloadBook(id: number, title: string): void {
+    this.http.get(`http://localhost:8080/api/books/download/${id}`, {
+      responseType: 'blob',
+    }).subscribe(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `${title}.pdf`;
+      a.click();
+      window.URL.revokeObjectURL(url);
+    });
   }
 }
 
