ce2f631441a373fb10d5162dbd837ed62055f488
gauhar <mdgauhar0007@gmail.com>
2025-04-22 10:10:56 +0530

    n11

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,10 @@ import { SearchComponent } from "./search/search.component";
 
 export const routes: Routes = [
     {
+        path: '',
+        component: HomeComponent 
+    },
+    {
         path:"add-book",
         component: AddBookComponent
     },
