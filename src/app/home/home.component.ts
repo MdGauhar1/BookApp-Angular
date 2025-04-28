4e95683c551e5c50b87e7af7c5e7e016931225e5
gauhar <mdgauhar0007@gmail.com>
2025-04-28 11:06:39 +0530

    n21

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,8 +11,8 @@ export interface Book {
 
 @Component({
   selector: 'app-home',
-  standalone: true,  // This tells Angular that this is a standalone component
-  imports: [CommonModule, RouterLink],  // Import CommonModule here
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -22,49 +22,49 @@ export class HomeComponent {
       title: 'The Great Gatsby',
       author: 'F. Scott Fitzgerald',
       description: 'A novel about the American dream, set in the Jazz Age.',
-      imageUrl: 'assets/gatsby.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: '1984',
       author: 'George Orwell',
       description: 'A dystopian novel exploring the dangers of totalitarianism.',
-      imageUrl: 'assets/1984.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: 'To Kill a Mockingbird',
       author: 'Harper Lee',
       description: 'A novel about racism, justice, and childhood in the Depression-era South.',
-      imageUrl: 'assets/mockingbird.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: 'Pride and Prejudice',
       author: 'Jane Austen',
       description: 'A classic novel about love, marriage, and social class in 19th-century England.',
-      imageUrl: 'assets/pride-and-prejudice.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: 'Moby-Dick',
       author: 'Herman Melville',
       description: 'A sailor’s obsession with hunting a giant white whale that symbolizes the elusive nature of revenge.',
-      imageUrl: 'assets/moby-dick.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: 'The Catcher in the Rye',
       author: 'J.D. Salinger',
       description: 'A story about teenage angst and alienation, told through the eyes of Holden Caulfield.',
-      imageUrl: 'assets/catcher-in-the-rye.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: 'The Great Gatsby',
       author: 'F. Scott Fitzgerald',
       description: 'A novel about the American dream, set in the Jazz Age.',
-      imageUrl: 'assets/gatsby.jpg'
+      imageUrl: '/assets/j.jpg'
     },
     {
       title: '1984',
       author: 'George Orwell',
       description: 'A dystopian novel exploring the dangers of totalitarianism.',
-      imageUrl: 'assets/1984.jpg'
+      imageUrl: '/assets/j.jpg'
     },
   ];
 }
